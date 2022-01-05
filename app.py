1dfd6e5f0514236d9be00c0e2006ee86e0dee13f
KomorowskiKuba <komorii2k@gmail.com>
2022-01-05 18:42:37 +0100

    Added hashing and security in login and register

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, EmailField
 from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'notverysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/data/database.db'
+
 Bootstrap(app)
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(60))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class LoginForm(FlaskForm):
@@ -31,7 +55,12 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1> Hello ' + form.username.data + ' </h1>'
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('home'))
+        return '<h1> Invalid username or password! </h1>'
 
     return render_template('login.html', form=form)
 
@@ -41,14 +70,29 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256', salt_length=32)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
         return '<h1> You registered as' + form.username.data + ' </h1>'
 
     return render_template('register.html', form=form)
 
 
+@login_required
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@login_required
+@app.route('/logout')
+def logout():
+    logout_user()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
