7af408330c2c9208d3d141f729e9ce8e7a74e143
KomorowskiKuba <komorii2k@gmail.com>
2022-01-06 16:58:37 +0100

    Added basic password management

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import sqlite3
+
+import flask_login
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, EmailField
-from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from forms import LoginForm, RegisterForm, PasswordForm
 
 app = Flask(__name__)
 
@@ -20,29 +21,30 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+conn = sqlite3.connect('/app/data/database.db')
+
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(30), unique=True)
+    master_password = db.Column(db.String(60))
+    page_password = db.relationship('PagePassword')
+
+
+class PagePassword(db.Model):
+    __tablename__ = 'pagepassword'
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100))
     password = db.Column(db.String(60))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=60)])
-    remember = BooleanField('Remember me')
-
-
-class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    email = EmailField('E-mail', validators=[InputRequired(), Email(message='Invalid e-mail!'), Length(min=5, max=30)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=60)])
 
 
 @app.route('/')
@@ -57,7 +59,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
-            if check_password_hash(user.password, form.password.data):
+            if check_password_hash(user.master_password, form.password.data):
                 login_user(user, remember=form.remember.data)
                 return redirect(url_for('home'))
         return '<h1> Invalid username or password! </h1>'
@@ -71,7 +73,7 @@ def register():
 
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method='sha256', salt_length=32)
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        new_user = User(username=form.username.data, email=form.email.data, master_password=hashed_password)
 
         db.session.add(new_user)
         db.session.commit()
@@ -81,14 +83,29 @@ def register():
     return render_template('register.html', form=form)
 
 
+@app.route('/home', methods=['GET', 'POST'])
 @login_required
-@app.route('/home')
 def home():
-    return render_template('home.html')
+    form = PasswordForm()
+
+    print(type(flask_login))
+    print('Current user id', flask_login.current_user.id)
+
+    passwords = PagePassword.query.filter_by(user_id=flask_login.current_user.id)
+
+    if form.validate_on_submit():
+        new_pagepassword = PagePassword(address=form.address.data, password=form.password.data,
+                                        user_id=flask_login.current_user.id)
+        db.session.add(new_pagepassword)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template('home.html', form=form, passwords=passwords)
 
 
-@login_required
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
 
---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, EmailField, IntegerField
+from wtforms.validators import InputRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=60)])
+    remember = BooleanField('Remember me')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    email = EmailField('E-mail', validators=[InputRequired(), Email(message='Invalid e-mail!'), Length(min=5, max=30)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=60)])
+
+
+class PasswordForm(FlaskForm):
+    address = StringField('Address', validators=[InputRequired(), Length(max=100)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=60)])
